3e531a3b12127e79e0d033a864473e6e2bb9393f
universe1x <gallyamov2003@icloud.com>
2024-11-27 16:54:00 +0800

    Переделанный хедер добавлены специализации локация и немного фронта

--- groups/models.py
+++ groups/models.py
@@ -4,7 +4,9 @@ from main.models import TelegramUser
 
 class Group(models.Model):
     telegram_user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField()
     skills = models.TextField(blank=True, null=True)
     link = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, null=True, blank=True)
